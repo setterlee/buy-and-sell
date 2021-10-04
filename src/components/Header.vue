91c9d2644ecf3cec31d67f9663e369a60de89f20
Marco Nunez <setterlee@gmail.com>
2021-10-04 11:45:26 -0300

    making Header component from existing layout

---
+++ src/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <q-header>
+    <div class="row no-wrap">
+      <q-toolbar class="col-4">
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          <span class="gt-sm">
+            Buy And Sell
+          </span>
+        </q-toolbar-title>
+      </q-toolbar>
+      <q-toolbar class="col-4">
+        <q-input v-model="searchText" bg-color="white" standout="bg-white text-secondary" rounded outlined dense class="q-my-sm search-input">
+          <template v-slot:append>
+            <q-icon name="search" @click="text = ''" class="cursor-pointer" />
+          </template>
+        </q-input>
+      </q-toolbar>
+      <q-toolbar class="col-4">
+      </q-toolbar>
+    </div>
+  </q-header>
+</template>
+
+<style scoped>
+.search-input {
+  width: 100%;
+  color: #fff
+}
+</style>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Header',
+  props: [
+    'leftDrawerOpen'
+  ],
+  data: function () {
+    return {
+      searchText: ''
+    }
+  },
+  methods: {
+    toggleLeftDrawer (event) {
+      this.$emit('toggleLeftDrawer', !this.leftDrawerOpen)
+    }
+  }
+})
+</script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,33 +1,7 @@
 <template>
   <q-layout view="hhh lpR fff">
-
     
-
-    <q-header>
-      <div class="row no-wrap">
-        <q-toolbar class="col-4">
-          <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
-          <q-toolbar-title>
-            <q-avatar>
-              <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-            </q-avatar>
-            Buy And Sell
-          </q-toolbar-title>
-        </q-toolbar>
-        <q-toolbar class="col-4">
-          <q-input color="secondary" standout="bg-white text-secondary" rounded outlined dense class="q-my-sm search-input">
-            <template v-slot:append>
-              <q-icon name="search" @click="text = ''" class="cursor-pointer" />
-            </template>
-          </q-input>
-        </q-toolbar>
-        <q-toolbar class="col-4">
-        </q-toolbar>
-        
-      </div>
-      
-    
-    </q-header>
+    <Header @toggleLeftDrawer="toggleLeftDrawer" :leftDrawerOpen="leftDrawerOpen" />
 
     <q-drawer v-model="leftDrawerOpen" side="left" overlay bordered >
       <!-- drawer content -->
@@ -48,27 +22,20 @@
   </q-layout>
 </template>
 
-<style scoped>
-
-.search-input {
-  width: 100%;
-  color: #fff
-}
-
-
-</style>
-
 <script>
 import { ref } from 'vue'
-
+import Header from 'components/Header.vue'
 export default {
+  components: {
+    Header
+  },
   setup () {
     const leftDrawerOpen = ref(false)
 
     return {
       leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
+      toggleLeftDrawer (value) {
+        leftDrawerOpen.value = value
       }
     }
   }
